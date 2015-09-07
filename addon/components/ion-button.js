1476a80fd4b83f106a6da957aa470fe4575ba01b
Chase McCarthy <chase@code0100fun.com>
2015-09-07 18:44:56 -0400

    Add template for ion-button and convert to glimmer

--- addon/components/ion-button.js
+++ addon/components/ion-button.js
@@ -1,20 +1,21 @@
 import Ember from 'ember';
+import layout from '../templates/components/ion-button';
+
+const STYLE_TYPES = ['block', 'full', 'small', 'large', 'outline', 'clear'];
+const STYLE_TYPE_PATHS = STYLE_TYPES.map((key) => `attrs.${key}`);
 
 export default Ember.GlimmerComponent.extend({
-  tagName: 'button',
-  classNames: ['button'],
-  classNameBindings: [
-    'kindClass',
-    'block:button-block',
-    'full:button-full',
-    'small:button-small',
-    'large:button-large',
-    'outline:button-outline',
-    'clear:button-clear'
-  ],
+  layout,
 
-  kindClass: Ember.computed('kind', function() {
-    let kind = this.get('kind');
+  styleClasses: Ember.computed.apply(null, STYLE_TYPE_PATHS.concat(function() {
+    return STYLE_TYPES.map((key) => {
+      const value = this.get(`attrs.${key}`);
+      return typeof(value) === 'undefined' || value === 'false' ? '' : `button-${key}`;
+    }).join(' ');
+  })),
+
+  kindClass: Ember.computed('attrs.kind', function() {
+    let kind = this.get('attrs.kind');
     if (kind) {
       return `button-${kind}`;
     }
