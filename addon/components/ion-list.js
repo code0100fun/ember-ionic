1ce9268802ea16df94aa0c93f0add5b1b1c61fde
Chase McCarthy <chase@code0100fun.com>
2015-09-07 17:05:41 -0400

    Add template for ion-list and convert to glimmer

--- addon/components/ion-list.js
+++ addon/components/ion-list.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
+import layout from '../templates/components/ion-list';
 
 export default Ember.GlimmerComponent.extend({
-  tagName: 'ion-list',
-  classNames: ['list'],
-  classNameBindings: [
-    'inset:list-inset'
-  ],
-
+  layout,
   init() {
     this._super(...arguments);
     this._items = Ember.A([]);
