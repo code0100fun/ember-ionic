cf7adfc0c1130f8a26c9602896752049fc85baae
Chase McCarthy <chase@code0100fun.com>
2015-09-07 17:50:36 -0400

    Convert ion-checkbox to glimmer

--- addon/components/ion-checkbox.js
+++ addon/components/ion-checkbox.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/ion-checkbox';
 
 export default Ember.GlimmerComponent.extend({
-  layout: layout, 
-  tagName: 'label',
-  classNames: ['item', 'item-checkbox'],
-
-  checked: null
+  layout: layout
 });
