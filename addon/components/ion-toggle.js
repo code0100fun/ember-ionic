b6f50b8b04c40377e385e65075c9786a51db8051
Chase McCarthy <chase@code0100fun.com>
2015-09-07 18:45:51 -0400

    Convert ion-toggle to glimmer

--- addon/components/ion-toggle.js
+++ addon/components/ion-toggle.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/ion-toggle';
 
 export default Ember.GlimmerComponent.extend({
-  layout: layout,
-  tagName: 'div',
-  classNames: ['item', 'item-toggle', 'toggle-large'],
-
-  checked: null
+  layout: layout
 });
