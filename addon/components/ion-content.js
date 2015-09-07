4addfb52c7c6b2d39d609d0c5c8bc15275aeb4e9
Chase McCarthy <chase@code0100fun.com>
2015-09-07 17:45:57 -0400

    Add template for ion-content and convert to glimmer

--- addon/components/ion-content.js
+++ addon/components/ion-content.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
+import layout from '../templates/components/ion-content';
 
 export default Ember.GlimmerComponent.extend({
-  tagName: 'ion-content',
-  classNames: ['scroll-content', 'overflow-scroll'], //  overflow-scroll
-  classNameBindings: [
-    'header:has-header',
-    'footer:has-footer'
-  ]
+  layout
 });
