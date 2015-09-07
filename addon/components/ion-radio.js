b908fdf7ea7e8ba571bd6874045ee05bf7a32fa4
Chase McCarthy <chase@code0100fun.com>
2015-09-07 16:50:03 -0400

    Update ion-radio template

--- addon/components/ion-radio.js
+++ addon/components/ion-radio.js
@@ -2,7 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/ion-radio';
 
 export default Ember.GlimmerComponent.extend({
-  layout: layout,
-  tagName: 'label',
-  classNames: ['item', 'item-radio']
+  layout
 });
