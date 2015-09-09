8811d4d7c42d2cacb45cbc61e4d3802d5ea6dfac
Chase McCarthy <chase@code0100fun.com>
2015-09-09 01:23:24 -0400

    Use attrClassPrefix for ion-button component

--- addon/components/ion-button.js
+++ addon/components/ion-button.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 import layout from '../templates/components/ion-button';
+import attrClassPrefix from '../helpers/attr-class-prefix';
 
-const STYLE_TYPES = ['block', 'full', 'small', 'large', 'outline', 'clear'];
-const STYLE_TYPE_PATHS = STYLE_TYPES.map((key) => `attrs.${key}`);
+const PREFIX_ATTRS = ['block', 'full', 'small', 'large', 'outline', 'clear'];
 
 export default Ember.GlimmerComponent.extend({
   layout,
 
-  styleClasses: Ember.computed.apply(null, STYLE_TYPE_PATHS.concat(function() {
-    return STYLE_TYPES.map((key) => {
-      const value = this.get(`attrs.${key}`);
-      return typeof(value) === 'undefined' || value === 'false' ? '' : `button-${key}`;
-    }).join(' ');
-  })),
+  styleClasses: attrClassPrefix('button-', PREFIX_ATTRS),
 
   kindClass: Ember.computed('attrs.kind', function() {
     let kind = this.get('attrs.kind');
