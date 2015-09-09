be4a7f517277a569416a84f93a20510985ee126d
Chase McCarthy <chase@code0100fun.com>
2015-09-09 01:02:17 -0400

    Convert ion-option-button to glimmer

--- addon/components/ion-option-button.js
+++ addon/components/ion-option-button.js
@@ -1,23 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/ion-option-button';
-import IonItem from './ion-item';
 
 export default Ember.GlimmerComponent.extend({
   layout: layout,
-  tagName: '',
-  kindClass: Ember.computed('kind', function() {
-    let kind = this.get('kind');
+
+  kindClass: Ember.computed('attrs.kind', function() {
+    let kind = this.get('attrs.kind');
     let output = 'button';
     if (kind) {
       output += ` button-${kind}`;
     }
     return output;
-  }),
-
-  init() {
-    this._super(...arguments);
-
-    let ionItemComponent = this.nearestOfType(IonItem);
-    ionItemComponent._registerOptionButton(this);
-  }
+  })
 });
